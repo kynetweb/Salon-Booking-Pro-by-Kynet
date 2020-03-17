4ad443a0b88438a4c57ba0be434b89e544f20e73
Shashi Kant <Skant743@gmail.com>
2020-03-17 14:29:35 +0530

    first commit

---
+++ includes/class-salonbookingprok-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       #
+ * @since      1.0.0
+ *
+ * @package    Salonbookingprok
+ * @subpackage Salonbookingprok/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Salonbookingprok
+ * @subpackage Salonbookingprok/includes
+ * @author     kynet Web <contact@kynetweb.com>
+ */
+class Salonbookingprok_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-salonbookingprok-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       #
+ * @since      1.0.0
+ *
+ * @package    Salonbookingprok
+ * @subpackage Salonbookingprok/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Salonbookingprok
+ * @subpackage Salonbookingprok/includes
+ * @author     kynet Web <contact@kynetweb.com>
+ */
+class Salonbookingprok_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
