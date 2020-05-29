79e5449d7c5a1c2139f21c86f0898632d8425d52
Rashmi <rashmicool60@gmail.com>
2020-05-29 15:04:48 +0530

    commit by rashmi

--- admin/class-sbprok-pages.php
+++ admin/class-sbprok-pages.php
@@ -66,7 +66,10 @@ class Sbprok_Pages {
 	 */
 	function menu_pages(){
 		add_menu_page(__('Salon Booking Pro', 'sbprok'), __('Salon Booking Pro', 'sbprok'), 'manage_options', 'sbprok', array($this, 'saloon_main_menu') );
-  	add_submenu_page('sbprok', __('Employees', 'sbprok'), 'Employees', 'manage_options', 'sbprok_employee_list',  array($this, 'employees_list')  );
+		if( current_user_can('sbprok_employee') ) {
+			add_menu_page(__('Calendar', 'sbprok'), __('Calendar', 'sbprok'), 'sbprok_employee', 'sbprok_calendar',array($this, 'calendar_sub_menu') );
+			}
+		add_submenu_page('sbprok', __('Employees', 'sbprok'), 'Employees', 'manage_options', 'sbprok_employee_list',  array($this, 'employees_list')  );
 		add_submenu_page('sbprok', __('Add Employee', 'sbprok'), __('Add Employee', 'sbprok'), 'manage_options', 'sbprok_add_employee',  array($this, 'add_employee')  );
 		add_submenu_page( 'sbprok', __('Services', 'sbprok'), __('Services', 'sbprok'), 'manage_options', 'edit.php?post_type=sbprok_services', NULL );
 		add_submenu_page( 'sbprok', __('Service Categories', 'sbprok'), __('Service Categories', 'sbprok'), 'manage_options', 'edit-tags.php?taxonomy=sbprok_category', NULL );
@@ -74,7 +77,10 @@ class Sbprok_Pages {
 		add_submenu_page('sbprok', __('Calendar', 'sbprok'), __('Calendar', 'sbprok'), 'manage_options', 'sbprok_calendar',  array($this, 'calendar_sub_menu')  );
 		add_submenu_page('sbprok', __('Settings', 'sbprok'), __('Settings', 'sbprok'), 'manage_options', 'saloon-setting-options',  array($this, 'saloon_setting_page')  );
 		remove_submenu_page('sbprok','sbprok');
+	
 	}
+
+   
 
    /**
 	 * callback calendar form sub menu functions.
